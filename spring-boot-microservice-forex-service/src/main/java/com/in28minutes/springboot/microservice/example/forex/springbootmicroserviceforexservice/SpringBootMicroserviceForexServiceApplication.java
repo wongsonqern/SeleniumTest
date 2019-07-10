5ce16a87bc45991fccdb4d76eebb0c6dabde3dff
SongErn <drinkpepsi@gmail.com>
2019-07-10 23:11:40 +0800

    test trigger

--- spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplication.java
+++ spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplication.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.microservice.example.forex.springbootmicroserviceforexservice;
 
+// testing trigger
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
